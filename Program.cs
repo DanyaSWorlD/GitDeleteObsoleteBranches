4e1f0d8b45bf6632153fc39bf9cc25c1feecf998
Daniil Mirenskii <bucashechca@gmail.com>
2024-03-14 01:02:27 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using GitDeleteObsoleteBranches;
+
+using Medallion.Shell;
+
+
+var spinner = new Spinner();
+spinner.Start();
+
+// Check if path is a git repository
+if (!await IsGitRepository(Environment.CurrentDirectory))
+    ExitWithError("Not a git repository");
+
+// Get all local branches
+var localBranchesResult = await GetCommandResult(Command.Run("git", "branch", "-a"));
+var localBranches = localBranchesResult
+    .Split(Environment.NewLine)
+    .Select(x => x.Trim())
+    .Where(x => !x.StartsWith("remotes") && !string.IsNullOrWhiteSpace(x))
+    .ToList();
+
+// Get local branches with non existing origins (deleted on remote)
+var prunedBranchesResult = await Command.Run("git", "fetch", "--prune", "--dry-run").Task;
+var prunedBranches = prunedBranchesResult.StandardError
+    .Split(Environment.NewLine)
+    .Where(x => x.StartsWith(" - "))
+    .Select(x => x.Split("->").Last().Trim().Replace("origin/", string.Empty))
+    .ToList();
+
+var branchesToRemove = prunedBranches.Where(x => localBranches.Contains(x)).ToList();
+spinner.Stop();
+
+Console.WriteLine("This branches are going to be removed:");
+foreach (var branch in branchesToRemove)
+    Console.WriteLine($" - {branch}");
+Console.WriteLine();
+
+Console.WriteLine("Delete this branches? Y/N:");
+var key = Console.ReadLine();
+Console.WriteLine();
+
+if (string.IsNullOrWhiteSpace(key) || !key.StartsWith("Y"))
+    ExitWithError("Abandoning cleanup");
+
+foreach (var branch in branchesToRemove)
+    Console.Write(await GetCommandResult(Command.Run("git", "branch", "-D", branch)));
+Console.WriteLine();
+
+Console.WriteLine("Performing git prune");
+
+spinner = new Spinner();
+spinner.Start();
+
+await Command.Run("git", "fetch", "--prune").Task;
+
+spinner.Stop();
+
+Console.WriteLine("Cleanup finished.");
+
+async Task<bool> IsGitRepository(string path)
+{
+    if (string.IsNullOrEmpty(path))
+        return false;
+
+    if (!Directory.Exists(path))
+        return false;
+
+    Directory.SetCurrentDirectory(path);
+
+    var commandOutput = await GetCommandResult(Command.Run("git", "rev-parse", "--is-inside-work-tree"));
+    commandOutput = commandOutput.Replace(Environment.NewLine, string.Empty);
+    if (commandOutput != true.ToString().ToLower())
+        return false;
+    return true;
+}
+
+async Task<string> GetCommandResult(Command command)
+{
+    var result = await command.Task;
+
+    return result.Success ? result.StandardOutput : result.StandardError;
+}
+
+void ExitWithError(string message)
+{
+    spinner?.Stop();
+    Console.WriteLine($"Error: {message}");
+    Environment.Exit(0);
+}
---
+++ Spinner.cs
@@ -0,0 +1,33 @@
+﻿namespace GitDeleteObsoleteBranches;
+
+internal class Spinner
+{
+    private readonly CancellationTokenSource _source = new();
+    private readonly int _spinnerPosition = Console.CursorLeft;
+
+    public void Start()
+    {
+        Task.Run(() => ShowLoading(_source.Token));
+    }
+
+    public void Stop()
+    {
+        _source.Cancel();
+        Console.CursorLeft = _spinnerPosition;
+    }
+
+    private async Task ShowLoading(CancellationToken token)
+    {
+        var states = new[] { "|", "/", "-", "\\" };
+        var index = 0;
+
+        while (!token.IsCancellationRequested)
+        {
+            var spinChar = states[index];
+            Console.CursorLeft = _spinnerPosition;
+            Console.Write(spinChar);
+            index = index + 1 == states.Length ? 0 : index + 1;
+            await Task.Delay(200);
+        }
+    }
+}
